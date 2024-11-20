9d56154b9e98d378855dc6da33136e4a9bfc0bde
김현래 <gusto917@naver.com>
2024-11-20 15:33:10 +0900

    Update vue-project/src/main.js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,6 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/css/global-background.css'; // 글로벌 CSS 임포트
+import './assets/css/global-component.css'; // 글로벌 CSS 임포트
+import './assets/css/global-layout.css'; // 글로벌 CSS 임포트
+import './assets/css/global-font.css'; // 글로벌 CSS 임포트
 
 const app = createApp(App)
 
