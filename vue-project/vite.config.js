83e29e3e555586ecb6368c5f6b0ee354814a49c3
김현래 <gusto917@naver.com>
2024-11-22 10:47:42 +0900

    포트번호 5173으로 고정

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 5173, // Vite 서버 포트를 5173으로 설정
+    strictPort: true, // 지정된 포트를 강제로 사용 (이미 사용 중이면 실패)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
